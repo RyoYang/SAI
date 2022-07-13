3c6679a99c26c229ccb875b682b9bf6ef087a1f2
Yang Wang <yangwang1@microsoft.com>
2022-07-13 09:36:10 +0000

    add

--- test/sai_test/sai_fdb_test.py
+++ test/sai_test/sai_fdb_test.py
@@ -103,7 +103,7 @@ class VlanPortLearnDisableTest(T0TestBase):
 
         send_packet(self, 2, self.chck_pkt)
         verify_each_packet_on_multiple_port_lists(
-            self, [self.chck_pkt, self.chck_pkt], [[self.dev_port_list[1]], [self.dev_port_list[3:9]]])
+            self, [self.chck_pkt], [self.dev_port_list[1], self.dev_port_list[3:9]])
 
         self.assertEqual(attr["available_fdb_entry"] - current_fdb_entry, 0)
         print("Verification complete")
@@ -243,11 +243,11 @@ class NewVlanmemberLearnTest(T0TestBase):
         """
         T0TestBase.setUp(self, is_reset_default_vlan=False)
         unknown_mac1 = "00:01:01:99:99:99"
-        self.pkt = simple_udp_packet(eth_dst=self.local_server_mac_list[1],
+        self.pkt1 = simple_udp_packet(eth_dst=self.local_server_mac_list[1],
                                     eth_src=unknown_mac1,
                                     pktlen=100)
-        self.chck_pkt = simple_udp_packet(eth_dst=unknown_mac1,
-                                    th_dst=self.local_server_mac_list[1],
+        self.pkt2 = simple_udp_packet(eth_dst=unknown_mac1,
+                                    eth_src=self.local_server_mac_list[1],
                                     pktlen=100)
         self.new_vlan10_member = sai_thrift_create_vlan_member(self.client,
             vlan_id=self.vlans[10].vlan_oid,
@@ -256,42 +256,198 @@ class NewVlanmemberLearnTest(T0TestBase):
 
         sai_thrift_set_port_attribute(
             self.client, self.port_list[24], port_vlan_id=10)
-                        
-        status = sai_thrift_remove_bridge_port(self.client, self.port_list[1])
         self.assertEqual(status, SAI_STATUS_SUCCESS)
 
 
     def runTest(self):
-        print("NoBPPortNoLearnTest")
-
+        print("NewVlanmemberLearnTest")
         attr = sai_thrift_get_switch_attribute(
             self.client, available_fdb_entry=True)
         current_fdb_entry = attr["available_fdb_entry"]
 
-        send_packet(self, 1, self.pkt)
-        verify_each_packet_on_multiple_port_lists(
-            self, [self.pkt], [self.dev_port_list[2:9]])
+        send_packet(self, 24, self.pkt1)
+        verify_packet(self, self.pkt1, self.dev_port_list[1])
 
-        send_packet(self, 2, self.chck_pkt)
-        verify_each_packet_on_multiple_port_lists(
-            self, [self.chck_pkt, self.chck_pkt], [[self.dev_port_list[1]], [self.dev_port_list[3:9]]])
+        send_packet(self, 1, self.chck_pkt2)
+        verify_packet(self, self.pkt2, self.dev_port_list[24])
+       
+        self.assertEqual(attr["available_fdb_entry"] - current_fdb_entry, 1)
+        print("Verification complete")
 
+
+    def tearDown(self):
+        pass
+
+
+class RemoveVlanmemberLearnTest(T0TestBase):
+    """
+    Verify no MAC addresses are learned on the removed vlan member
+    """
+
+    def setUp(self):
+        """
+        Test the basic setup process
+        """
+        T0TestBase.setUp(self, is_reset_default_vlan=False)
+        unknown_mac1 = "00:01:01:99:99:99"
+        self.pkt1 = simple_udp_packet(eth_dst=self.local_server_mac_list[1],
+                                    eth_src=unknown_mac1,
+                                    vlan_vid=10,
+                                    pktlen=100)
+        self.pkt2 = simple_udp_packet(eth_dst=unknown_mac1,
+                                    eth_src=self.local_server_mac_list[1],
+                                    vlan_vid=10,
+                                    pktlen=100)
+
+        sai_thrift_set_port_attribute(
+            self.client, self.port_list[24], port_vlan_id=0)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+
+    def runTest(self):
+        print("RemoveVlanmemberLearnTest")
+        attr = sai_thrift_get_switch_attribute(
+            self.client, available_fdb_entry=True)
+        current_fdb_entry = attr["available_fdb_entry"]
+
+        send_packet(self, 2, self.pkt1)
+        verify_no_other_packets(self)
+
+        send_packet(self, 1, self.chck_pkt2)
+        verify_packet(self, self.pkt2, self.dev_port_list[2:9])
+       
         self.assertEqual(attr["available_fdb_entry"] - current_fdb_entry, 0)
         print("Verification complete")
 
 
     def tearDown(self):
-        sai_thrift_flush_fdb_entries(
-            self.client, entry_type=SAI_FDB_FLUSH_ENTRY_TYPE_ALL)
+        pass
 
-        # restore initial bridge port
-        self.bridge_port_list[1] = sai_thrift_create_bridge_port(
-            self.client,
-            bridge_id=self.default_1q_bridge_id,
-            port_id=self.port_list[1],
-            type=SAI_BRIDGE_PORT_TYPE_PORT,
-            admin_state=True)
 
+class InvalidateVlanmemberNoLearnTest(T0TestBase):
+    """
+    Verify no MAC addresses are learned on the removed vlan member
+    """
+
+    def setUp(self):
+        """
+        Test the basic setup process
+        """
+        T0TestBase.setUp(self, is_reset_default_vlan=False)
+        unknown_mac1 = "00:01:01:99:99:99"
+        self.pkt1 = simple_udp_packet(eth_dst=self.local_server_mac_list[1],
+                                    eth_src=unknown_mac1,
+                                    vlan_vid=11,
+                                    pktlen=100)
+        self.pkt2 = simple_udp_packet(eth_dst=unknown_mac1,
+                                    eth_src=self.local_server_mac_list[1],
+                                    vlan_vid=11,
+                                    pktlen=100)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+
+    def runTest(self):
+        print("RemoveVlanmemberLearnTest")
+        attr = sai_thrift_get_switch_attribute(
+            self.client, available_fdb_entry=True)
+        current_fdb_entry = attr["available_fdb_entry"]
+
+        send_packet(self, 2, self.pkt1)
+        verify_no_other_packets(self)
+
+        send_packet(self, 1, self.chck_pkt2)
+        verify_no_other_packets(self)
+       
+        self.assertEqual(attr["available_fdb_entry"] - current_fdb_entry, 0)
+        print("Verification complete")
+
+
+    def tearDown(self):
+        pass
+
+
+class BroadcastNoLearnTest(T0TestBase):
+    """
+    Verify no MAC addresses are learned on the removed vlan member
+    """
+
+    def setUp(self):
+        """
+        Test the basic setup process
+        """
+        T0TestBase.setUp(self, is_reset_default_vlan=False)
+        bcast_mac = "ff:ff:ff:ff:ff:ff"
+        self.pkt1 = simple_udp_packet(eth_src=bcast_mac,
+                                    pktlen=100)
+
+        self.pkt2 = simple_udp_packet(eth_dst=bcast_mac,
+                                    pktlen=100)
+
+        sai_thrift_set_port_attribute(
+            self.client, self.port_list[24], port_vlan_id=0)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+
+    def runTest(self):
+        print("BroadcastNoLearnTest")
+        attr = sai_thrift_get_switch_attribute(
+            self.client, available_fdb_entry=True)
+        current_fdb_entry = attr["available_fdb_entry"]
+
+        send_packet(self, 2, self.pkt1)
+        verify_no_other_packets(self)
+
+        send_packet(self, 1, self.chck_pkt2)
+        verify_packet(self, self.pkt2, self.dev_port_list[2:9])
+       
+        self.assertEqual(attr["available_fdb_entry"] - current_fdb_entry, 0)
+        print("Verification complete")
+
+
+    def tearDown(self):
+        pass
+
+
+class MulticastNoLearnTest(T0TestBase):
+    """
+    Verify no MAC addresses are learned on the removed vlan member
+    """
+
+    def setUp(self):
+        """
+        Test the basic setup process
+        """
+        T0TestBase.setUp(self, is_reset_default_vlan=False)
+        mcast_mac = "01:00:5e:11:22:33"
+        self.pkt1 = simple_udp_packet(eth_src=mcast_mac,
+                                    pktlen=100)
+
+        self.pkt2 = simple_udp_packet(eth_dst=mcast_mac,
+                                    pktlen=100)
+
+        sai_thrift_set_port_attribute(
+            self.client, self.port_list[24], port_vlan_id=0)
+        self.assertEqual(status, SAI_STATUS_SUCCESS)
+
+
+    def runTest(self):
+        print("MulticastNoLearnTest")
+        attr = sai_thrift_get_switch_attribute(
+            self.client, available_fdb_entry=True)
+        current_fdb_entry = attr["available_fdb_entry"]
+
+        send_packet(self, 2, self.pkt1)
+        verify_no_other_packets(self)
+
+        send_packet(self, 1, self.chck_pkt2)
+        verify_packet(self, self.pkt2, self.dev_port_list[2:9])
+       
+        self.assertEqual(attr["available_fdb_entry"] - current_fdb_entry, 0)
+        print("Verification complete")
+
+
+    def tearDown(self):
+        pass
 
 
 class FdbPortAgingTest(T0TestBase):
